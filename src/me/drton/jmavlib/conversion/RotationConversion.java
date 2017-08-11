2f12f44d9938ca4eae72d5aec491d81c98861fa4
Andreas Antener <antener_a@gmx.ch>
2017-08-11 08:27:14 +0200

    Added rotation matrices analogue to PX4

--- src/me/drton/jmavlib/conversion/RotationConversion.java
+++ src/me/drton/jmavlib/conversion/RotationConversion.java
@@ -1,9 +1,9 @@
 package me.drton.jmavlib.conversion;
 
+import javax.vecmath.Matrix3d;
 import javax.vecmath.Vector3d;
 
-import static java.lang.Math.cos;
-import static java.lang.Math.sin;
+import static java.lang.Math.*;
 
 /**
  * User: ton Date: 02.06.13 Time: 20:20
@@ -23,11 +23,43 @@ public class RotationConversion {
         };
     }
 
+    public static double[] rotationMatrixByQuaternion(double[] q) {
+        return new double[]{
+                q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3],
+                2 * (q[1] * q[2] - q[0] * q[3]),
+                2 * (q[0] * q[2] + q[1] * q[3]),
+                2 * (q[1] * q[2] + q[0] * q[3]),
+                q[0] * q[0] - q[1] * q[1] + q[2] * q[2] - q[3] * q[3],
+                2 * (q[2] * q[3] - q[0] * q[1]),
+                2 * (q[1] * q[3] - q[0] * q[2]),
+                2 * (q[0] * q[1] + q[2] * q[3]),
+                q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]
+        };
+    }
+
     public static double[] eulerAnglesByQuaternion(double[] q) {
         return new double[]{
                 Math.atan2(2.0 * (q[0] * q[1] + q[2] * q[3]), 1.0 - 2.0 * (q[1] * q[1] + q[2] * q[2])),
                 Math.asin(2 * (q[0] * q[2] - q[3] * q[1])),
                 Math.atan2(2.0 * (q[0] * q[3] + q[1] * q[2]), 1.0 - 2.0 * (q[2] * q[2] + q[3] * q[3])),
+        };
+    }
+
+    public static double[] eulerAnglesByRotationMatrix(Matrix3d matrix) {
+        double phi_val = atan2(matrix.getM21(), matrix.getM22());
+        double theta_val = asin(-matrix.getM20());
+        double psi_val = atan2(matrix.getM10(), matrix.getM00());
+
+        if (abs(theta_val - PI/2) < 1.0e-3) {
+            phi_val = 0.0;
+            psi_val = atan2(matrix.getM12(), matrix.getM02());
+        } else if (abs(theta_val + PI/2) < 1.0e-3) {
+            phi_val = 0.0;
+            psi_val = atan2(-matrix.getM12(), -matrix.getM02());
+        }
+
+        return new double[] {
+                phi_val, theta_val, psi_val
         };
     }
 
